703eb36a266f35650ca4d5f750767b324686329a
Juan Pablo Kaniefsky <jpkaniefsky@gmail.com>
2019-10-26 00:45:42 -0300

    ssr cookies initialization fixed

--- src/client/modules/reducer.ts
+++ src/client/modules/reducer.ts
@@ -3,15 +3,19 @@ import { types } from './actions'
 import authReducer from './auth/reducer'
 import tasksReducer from './tasks/reducer'
 
+const notifyError = error => {
+	if (typeof document === 'undefined') return
+	console.error(error, error.response)
+	alert(error.response.data.message)
+}
+
 export default combineReducers({
 	auth: authReducer,
 	tasks: tasksReducer,
 	common: (state, action) => {
 		switch (action.type) {
 			case types.requestFailure:
-				const { error } = action.payload
-				console.error(error, error.response)
-				alert(error.response.data.message)
+				notifyError(action.payload.error)
 				return state
 			default:
 				return state
